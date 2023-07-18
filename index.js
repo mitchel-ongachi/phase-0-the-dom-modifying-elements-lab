a6d09cdb3c9a40a4ce24ecfdae776b4c7918c8c5
mitchel buyengo <mitchelongachi63@gmail.com>
2023-07-18 19:55:51 +0300

    work done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Martin is the champion";
+
+
+document.body.appendChild(newHeader);
